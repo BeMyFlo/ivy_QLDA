f11961ea0a21ea069eba41a91c57a36abf4cd6c0
ThongHa <haminhthong0811@gmail.com>
2022-11-29 09:22:39 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,39 @@
+    <!-- -------------------------Footer -->
+    <section class="footer">
+        <div class="footer-container">
+            <p>Tải ứng dụng IVY moda</p>
+            <div class="app-google">
+                <a href="https://apps.apple.com/us/app/ivy-moda/id1430094474"><img src="image/appstore.jpg" alt=""></a>
+                <a href="https://play.google.com/store/apps/details?id=com.ivymoda.app&hl=en_US&gl=US"> <img src="image/googleplay.jpg" alt=""></a>
+            </div>
+            <p>Nhận bản tin IVY moda</p>
+            <div class="input-email">
+                <input type="text" placeholder="Nhập email của bạn">
+                <i class="fas fa-arrow-left"></i>
+            </div>
+            <div class="footer-items">
+                <li><a href=""><img src="image/dathongbao.png" alt=""></a></li>
+                <li><a href="">Liên hệ</a></li>
+                <li><a href="">Tuyển dụng</a></li>
+                <li><a href="">Giới thiệu</a></li>
+                <li><a href=""><i class="fab fa-facebook-f"></i></a><a href=""><i class="fab fa-youtube"></i></a></li>
+            </div>
+            <div class="footer-text">
+                Công ty Cổ phần 3TIvy: 0934149864 <br>
+Địa chỉ đăng ký: Đại học Công Nghệ Thông Tin - 0934149864 <br>
+Đặt hàng online : 0934149864.
+            </div>
+            <div class="footer-bottom">
+                ©Ivy3T
+            </div>
+        </div>
+    </section>
+
+
+    <script src="js/slider.js"></script>
+    <script src="js/script.js"></script>
+
+
+
+</body>
+</html>
---
+++ slider.php
@@ -0,0 +1,57 @@
+    <!-- -----------------------SLlDER---------------------------------------------- -->
+    
+    <section class="sliders">
+        <div class="aspect-ratio-169">
+            <img src="../ivyfullstak/image/slide1.jpg" alt="">
+            <img src="../ivyfullstak/image/slide2.jpg" alt="">
+            <img src="../ivyfullstak/image/slide3.jpg" alt="">
+        </div>
+        <div class="dot-container">
+            <div class="dot active"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+       </div>
+</section>
+<!-- -----------------------------------slider----------------------- -->
+<!--San pham ARRIVAL-->
+<div class="title">
+NEW ARRIVAL
+</div>
+    <section class="cartegory">
+    <div class="container">
+        <div class="cartegory-right-content row">
+            <div class="cartegory-right-content-item">
+                <img src="../ivyfullstak/image/sp1.jpg" alt="">
+                    <h1><a>ZUÝP LỤA ĐUÔI CÁ MS 31T0014</a></h1>
+                    <p>790.000<sup>đ</sup></p>
+            </div>
+            <div class="cartegory-right-content-item">
+                <img src="../ivyfullstak/image/sp2.jpg" alt="">
+                <h1><a>ZUÝP LỤA ĐUÔI CÁ MS 31T0014</a></h1>
+                <p>790.000<sup>đ</sup></p>
+            </div>
+            <div class="cartegory-right-content-item">
+                <img src="../ivyfullstak/image/sp3.jpg" alt="">
+                <h1><a>ÝP LỤA ĐUÔI CÁ MS 31T0014</a></h1>
+                <p>790.000<sup>đ</sup></p>
+            </div>
+            <div class="cartegory-right-content-item">
+                <img src="../ivyfullstak/image/sp4.jpg" alt="">
+                <h1><a>ZUÝP LỤA ĐUÔI CÁ MS 31T0014</a></h1>
+                <p>790.000<sup>đ</sup></p>
+            </div>
+        </div>
+    </div>
+    <div class="button-xemtatca">
+    <button class="custom-btn btn-12"><span>Click!</span><span>Xem tất cả</span></button>
+    </div>
+    </section>
+    <!-- Anh quang cao BST-->
+    <div class="title" style = "font-family:Sans-serif;">
+    NEW COLLECTION
+    </div>
+    <div class="trangchuBST">
+    <div class="aspect-ratio-170">
+        <img id="img"  src="../ivyfullstak/image/trangchu.jpg">
+    </div>
+</div>
